dfa854ce77f38888203f0a5c8a6a2bf0e4042813
Arctostaphylos <shanks@sonic.net>
2014-05-29 17:48:41 -0700

    final release

--- week_8_and_9/2_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/2_JavaScript/jquery/jquery_example.js
@@ -14,15 +14,26 @@ $('body').css({'background-color': 'pink'})
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
  $('h1').css({'color': 'red', 'border': 'solid black 5px'})
+ $("div h1").html("<h1>Chorus Frogs</h1>")
 
  
 //RELEASE 3: Event Listener
   // Add the code for the event listener here 
- 
+ $('img').on('mouseover', function(e){
+ 	e.preventDefault()
+ 	$(this).attr('src', 'http://soundofcritters.com/wp-content/uploads/2010/04/pacific-chorus-frog.jpg')
+ })
+
+ $('img').on('mouseleave', function(){
+ 	$(this).attr('src', "dbc_logo.jpg")
+ })
  
 //RELEASE 4 : Experiment on your own
  
- 
+ $('div img').css({'border': 'solid blue 5px'})
+ $(document).ready(function(){
+ 	$('div img').animate({borderWidth:'50px'});
+ });
  
  
  
